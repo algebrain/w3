fdc68326fff3f9dd3a4dc750c2f0765afe212a8f
bosm <bosm@bosmbosm.com>
2024-04-25 01:50:27 +0300

    checking query params added to tests for CompileSelect

--- w3sql/read_test.go
+++ w3sql/read_test.go
@@ -68,6 +68,14 @@ order by name DESC`
 	fmt.Println("Query:", qs[0])
 	fmt.Println("Params:", p)
 
+	if len(p) != 1 {
+		t.Fatal("1 parameter expected, got", len((p)))
+	}
+
+	if x, ok := p["sqv0"].(float64); !ok || x != 23 {
+		t.Fatal("expected 23 for age, got", fmt.Sprintf("<%v> <%T> <%v>", x, p["sqv0"], ok))
+	}
+
 	expectedQS = `
 select * from students where score > 50
 and (age::int<=:sqv0)
@@ -140,6 +148,23 @@ func TestCompileCompoundSelect(t *testing.T) {
 	if len(qs) != 1 {
 		t.Fatal("1 sql query expected, got", len((qs)))
 	}
+
+	if len(p) != 3 {
+		t.Fatal("3 parameters expected, got", len((p)))
+	}
+
+	values := map[string]int{"23": 1, "Bob": 1, "Alice": 1}
+	for _, v := range p {
+		vv := fmt.Sprint(v)
+		if _, ok := values[vv]; !ok {
+			t.Fatal("no such value expected", v)
+		}
+		delete(values, vv)
+	}
+	if len(values) != 0 {
+		t.Fatal("not all expected values are received: ", values)
+	}
+
 	fmt.Println("Query:", qs[0])
 	fmt.Println("Params:", p)
 
@@ -173,6 +198,79 @@ order by name DESC`
 	expectedQS = `
 select * from students where score > 50
 and ((age::int<=:sqv0) AND ((name LIKE '%' || :sqv1 || '%') OR (name LIKE :sqv2 || '%')))
+limit 10
+offset 20
+order by name DESC`
+
+	if !equalSQLStrings(expectedQS, qs[0]) {
+		t.Fatal(
+			"unexpected sql string result, got:",
+			fmt.Sprintf("<%s>", qs[0]),
+			"\nexpected",
+			fmt.Sprintf("<%s>", expectedQS),
+		)
+	}
+}
+
+func TestCompileNeedWherePrepared(t *testing.T) {
+	limit := 10
+	offset := 20
+	q := Query{
+		Limit:  &limit,
+		Offset: &offset,
+		Search: &AtomaryCondition{
+			Field: "age",
+			Type:  "int",
+			Value: 23,
+			Op:    "<=",
+		},
+		Sort: []SortQuery{
+			SortQuery{
+				Field:     "name",
+				Direction: "desc",
+			},
+		},
+	}
+
+	cq, err := q.CompileSelect("sqlite", map[string]string{"age": "age::int", "name": ""})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	base := ""
+	qs, _, err := cq.SQL(base, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(qs) != 1 {
+		t.Fatal("1 sql query expected, got", len((qs)))
+	}
+	fmt.Println("Query:", qs[0])
+	expectedQS := `
+where (age::int<=:sqv0)
+limit 10
+offset 20
+order by name DESC`
+
+	if !equalSQLStrings(expectedQS, qs[0]) {
+		t.Fatal(
+			"unexpected sql string result, got:",
+			fmt.Sprintf("<%s>", qs[0]),
+			"\nexpected",
+			fmt.Sprintf("<%s>", expectedQS),
+		)
+	}
+
+	qs, _, err = cq.SQL(base, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(qs) != 1 {
+		t.Fatal("1 sql query expected, got", len((qs)))
+	}
+	fmt.Println("Query:", qs[0])
+	expectedQS = `
+and (age::int<=:sqv0)
 limit 10
 offset 20
 order by name DESC`
