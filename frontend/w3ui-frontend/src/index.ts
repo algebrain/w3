e90ff0c67b1ebbd0feb2fa8e94da3b3ccf818624
bosm <bosm@bosmbosm.com>
2024-05-23 02:44:37 +0300

    AtomaryConditionBuilder started

--- frontend/w3ui-frontend/src/index.ts
+++ frontend/w3ui-frontend/src/index.ts
@@ -2,6 +2,22 @@ export type Op = "==" | "or" | "<=" | ">=" | "<" | ">" | ">= or 0" | "!=" |
   "between" | "reverse in" | "in" | "not in" | "starts with" | "contains" | 
   "ends with";
 
+export type TypeName = "text" | "textis" | "list" | "string" | "number" | "int" | "float" | "date" | "datetime";
+
+type RangeTypeName = "date" | "datetime" | "numeric";
+
+export const NumericTypes: ReadonlySet<TypeName> = new Set([
+  "number", "int", "float"
+]);
+
+export const TimeTypes: ReadonlySet<TypeName> = new Set([
+  "date", "datetime"
+]);
+
+export const TextTypes: ReadonlySet<TypeName> = new Set([
+  "text", "textis", "string"
+]);
+
 export type Value = number | string | boolean;
 
 export interface AtomaryCondition {
@@ -45,3 +61,71 @@ export interface Query {
 }
 
 
+class AtomaryConditionBuilder {
+  private type_: TypeName | RangeTypeName;
+  private cond_?: AtomaryCondition;
+
+  constructor(t: TypeName) {
+    this.type_ = t;
+  };
+
+  op(op: Op, col: string, val: Value | Value[]) {
+    if (op === "between") {
+      if (NumericTypes.has(this.type_ as TypeName)) {
+        this.type_ = "numeric";
+        if (typeof val !== "number") throw new Error("[AtomaryQueryBuilder.op] number expected");
+        this.cond_ = {
+          Col:  col,
+          Type: this.type_,
+          Val:  val,
+          Op:   op,
+        };
+        return;
+      }
+      
+      if (!TimeTypes.has(this.type_ as TypeName)) {
+        throw new Error("[AtomaryQueryBuilder.op] date or number type of condition expected");
+      }
+
+      if (typeof val !== "string") throw new Error("[AtomaryQueryBuilder.op] string for date expected");
+
+      this.cond_ = {
+        Col:  col,
+        Type: this.type_,
+        Val:  val,
+        Op:   op,
+      };
+
+      return;
+    }
+
+    switch (this.type_) {
+      case "text":
+      case "textis":
+      case "string":
+        if (typeof val !== "string") throw new Error("[AtomaryQueryBuilder.op] string value expected");
+        break;
+      case "list":
+        if (!Array.isArray(val)) throw new Error("[AtomaryQueryBuilder.op] array value expected");
+        break;
+      case "number":
+      case "int":
+      case "float":
+        if (typeof val !== "number") throw new Error("[AtomaryQueryBuilder.op] number value expected");
+        break;
+      case "date":
+      case "datetime":
+        if (typeof val !== "string") throw new Error("[AtomaryQueryBuilder.op] string value expected");
+        break;
+    }
+ 
+    this.cond_ = {
+      Col:  col,
+      Type: this.type_,
+      Val:  val,
+      Op:   op,
+    };
+  }
+
+  get cond() { return this.cond_; }
+}
